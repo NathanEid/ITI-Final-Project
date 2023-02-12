6139d75f087af70c391059ccebe66b0e7cd8f192
NathanEid <nesoeid4@gmail.com>
2023-02-12 17:56:32 +0200

    adding rout table terraform module

---
+++ IAC-Terraform/routetable/main.tf
@@ -0,0 +1,21 @@
+resource "aws_route_table" "route_table" {
+  vpc_id = var.table_vpc_id
+  tags = {
+    Name = var.table_name
+  }
+}
+
+resource "aws_route" "route" {
+  route_table_id = aws_route_table.route_table.id
+  destination_cidr_block = var.table_destination_cidr_block
+  gateway_id = var.table_gateway_id
+  depends_on = [aws_route_table.route_table]
+}
+
+resource "aws_route_table_association" "subnet_association" {
+  # for_each = var.table_subnet_id
+  # subnet_id      = each.value
+  subnet_id = var.table_subnet_id
+  route_table_id = aws_route_table.route_table.id
+  depends_on = [aws_route_table.route_table]
+}
---
+++ IAC-Terraform/routetable/variables.tf
@@ -0,0 +1,17 @@
+variable "table_vpc_id" {
+  type = string
+}
+
+variable "table_name" {
+    type = string
+}
+
+variable "table_destination_cidr_block" {
+}
+
+variable "table_gateway_id" {
+}
+
+variable "table_subnet_id" {
+}
+
