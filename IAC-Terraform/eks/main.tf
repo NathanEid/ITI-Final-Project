0ff3c8b9ffc6cd5d8861d8987635ce59cb559718
NathanEid <nesoeid4@gmail.com>
2023-02-13 00:49:28 +0200

    adding EKS terraform module

---
+++ IAC-Terraform/eks/main.tf
@@ -0,0 +1,120 @@
+resource "aws_iam_role" "eks-iam-role" {
+ name = "devopsthehardway-eks-iam-role"
+
+ path = "/"
+
+ assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+  {
+   "Effect": "Allow",
+   "Principal": {
+    "Service": "eks.amazonaws.com"
+   },
+   "Action": "sts:AssumeRole"
+  }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+ policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+ role    = aws_iam_role.eks-iam-role.name
+}
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
+ policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+ role    = aws_iam_role.eks-iam-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
+ policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+ role    = aws_iam_role.eks-iam-role.name
+}
+
+
+
+resource "aws_eks_cluster" "devopsthehardway-eks" {
+ name = "devopsthehardway-cluster"
+ role_arn = aws_iam_role.eks-iam-role.arn
+
+
+
+ vpc_config {
+  subnet_ids = [var.subnet_id_1, var.subnet_id_2]
+  endpoint_private_access = true
+  endpoint_public_access  = false
+ }
+
+ depends_on = [
+  aws_iam_role.eks-iam-role,
+ ]
+}
+
+
+
+resource "aws_iam_role" "workernodes" {
+  name = "eks-node-group-example"
+ 
+  assume_role_policy = jsonencode({
+   Statement = [{
+    Action = "sts:AssumeRole"
+    Effect = "Allow"
+    Principal = {
+     Service = "ec2.amazonaws.com"
+    }
+   }]
+   Version = "2012-10-17"
+  })
+ }
+ 
+ resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role    = aws_iam_role.workernodes.name
+ }
+ 
+ resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role    = aws_iam_role.workernodes.name
+ }
+ 
+ resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
+  policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
+  role    = aws_iam_role.workernodes.name
+ }
+ 
+ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role    = aws_iam_role.workernodes.name
+ }
+
+
+
+
+resource "aws_eks_node_group" "worker-node-group" {
+  cluster_name  = aws_eks_cluster.devopsthehardway-eks.name
+  node_group_name = "devopsthehardway-workernodes"
+  node_role_arn  = aws_iam_role.workernodes.arn
+  subnet_ids   = [var.subnet_id_1, var.subnet_id_2]
+  instance_types = ["t2.micro"]
+  disk_size = 10
+
+#   remote_access {
+#     ec2_ssh_key = "mykey"
+#     source_security_group_ids = [ "All" ]
+#   }
+ 
+  scaling_config {
+   desired_size = 1
+   max_size   = 1
+   min_size   = 1
+  }
+ 
+  depends_on = [
+   aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+   aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+   aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+   aws_iam_role_policy_attachment.EC2InstanceProfileForImageBuilderECRContainerBuilds
+  ]
+ }
---
+++ IAC-Terraform/eks/variables.tf
@@ -0,0 +1,5 @@
+variable "subnet_id_1" {
+}
+
+variable "subnet_id_2" {
+}
